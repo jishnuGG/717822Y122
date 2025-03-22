0ba2c1262ea2ad634d708e9d27e84b530b6c4663
jishnuGG <166325684+jishnuGG@users.noreply.github.com>
2025-03-22 12:40:49 +0530

    updated

--- average-calculator/src/App.js
+++ average-calculator/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import AverageCalculator from "./AverageCalculator";
 import { Container } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
-import reportWebVitals from "./reportWebVitals";
+
 function App() {
   return (
     <Container>
-      <AverageCalculator />
+      <h1>Welcome to Average Calculator</h1>
     </Container>
   );
 }
